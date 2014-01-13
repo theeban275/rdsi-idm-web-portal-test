9a9f109346906a073046b9ced267a5b791fa5fec
Theeban Soundararajan <theeban@intersect.org.au>
2014-01-13 14:59:44 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RdsiIdmWeb::Application.config.secret_token = 'f4020be4ce48b6148cfdcde9dd8d52e573119c2352cafeba76a132ec15a823da7c756fd8defd9ec41217dfc1909a1d8ebb580821221f7ee346fb8b5cfee3153a'
